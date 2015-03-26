064e1f8fe2a761ecd7af063fd270f80dc3763255
Ricardo Perez <riperezzz@gmail.com>
2015-03-26 22:44:10 +0000

    adding specs covering command inputs and output

--- spec/lib/application_spec.rb
+++ spec/lib/application_spec.rb
@@ -1,33 +1,47 @@
 require 'spec_helper'
 
-#> I 5 6
-#> L 2 3 A
-#> S
-#OOOOO
-#OOOOO
-#OAOOO
-#OOOOO
-#OOOOO
-#OOOOO
-#> F 3 3 J
-#> V 2 3 4 W
-#> H 3 4 2 Z
-#> S
-#JJJJJ
-#JJZZJ
-#JWJJJ
-#JWJJJ
-#JJJJJ
-#JJJJJ
 module BitmapEditor
   describe Application do
     subject { described_class.new }
 
     describe 'showing bitmap' do
+
       context 'after a sets of commands' do
-        context 'and the output command' do
+        let(:command1) { 'I 5 6' }
+        let(:command2) { 'L 2 3 A' }
+
+        context 'and request output' do
+          let(:command3) { 'S' }
+
+          it 'shows' do
+            "OOOOO
+            OOOOO
+            OAOOO
+            OOOOO
+            OOOOO
+            OOOOO"
+          end
         end
       end
+
+      context 'another set of commands' do
+        let(:command1) { 'F 3 3 J' }
+        let(:command2) { 'V 2 3 4 W' }
+        let(:command3) { 'H 3 4 2 Z' }
+
+        context 'and request output' do
+          let(:command3) { 'S' }
+          it 'shows' do
+            "JJJJJ
+            JJZZJ
+            JWJJJ
+            JWJJJ
+            JJJJJ
+            JJJJJ"
+          end
+        end
+      end
+
     end
   end
 end
