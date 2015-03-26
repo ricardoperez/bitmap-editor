9c3f77568834ac78dc0fd357940ab92a5222751c
Ricardo Perez <riperezzz@gmail.com>
2015-03-26 22:50:51 +0000

    adding spec for command handling

--- spec/lib/bitmap-editor/command_parser_spec.rb
+++ spec/lib/bitmap-editor/command_parser_spec.rb
@@ -1,4 +1,10 @@
 module BitmapEditor
   describe CommandParser do
+    describe 'handling commands' do
+      context 'when command is I' do
+        it 'create empty matrix' do
+        end
+      end
+    end
   end
 end
