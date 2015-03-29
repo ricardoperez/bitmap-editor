29a7f9cd60c36b3fc09e4a3d4cc3180e4cca43fc
Ricardo Perez <riperezzz@gmail.com>
2015-03-29 22:13:40 +0100

    adjusments specs, creating CmdParser and Canvas class

--- lib/bitmap-editor.rb
+++ lib/bitmap-editor.rb
@@ -1,4 +1,6 @@
 require './lib/bitmap-editor/application'
+require './lib/bitmap-editor/command_parser'
+require './lib/bitmap-editor/matrix_canvas'
 
 module BitmapEditor
 end
--- spec/lib/bitmap-editor/matrix_canvas_spec.rb
+++ spec/lib/bitmap-editor/matrix_canvas_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module BitmapEditor
   describe MatrixCanvas do
 
-    decribe 'creating a empty canvas' do
+    describe 'creating a empty canvas' do
       context 'when create a matrix of 5 row and 6 lines' do
         it 'outputs' do
           "OOOOO
